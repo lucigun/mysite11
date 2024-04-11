85d5763ba0b9392fd5ca0a1f3f9683502ee28c6f
이성 <hughlee@idstrust.com>
2024-04-12 08:02:33 +0900

    2-04 조회와 템플릿 404페이지는 실현이 안됨

--- pybo/views.py
+++ pybo/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request) :
-    return HttpResponse("안녕 파이보")
+    question_list = Question.objects.order_by('-create_date')
+    context = {'question_list' : question_list}
+    return render(request, '/question_list.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question' : question}
+    return render(request, '/question_detail.html', context)
